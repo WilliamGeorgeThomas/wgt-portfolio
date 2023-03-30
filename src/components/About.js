eb57f9a9f4de4f385348de4a1f48bcaec3a8ae63
WGT <williamgeorgethomas@gmail.com>
2023-03-30 18:25:43 -0500

    updated headshot to one without flag.  replaced paper texture static background with color gradient that displays better on mobile screens

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,7 @@
 import React from "react";
-import flagHeadshot from "../assets/img/flagHeadshot.jpg";
+// import flagHeadshot from "../assets/img/flagHeadshot.jpg";
+import headshot from "../assets/img/conservatoryHeadshotlinkedin.jpg"
+
 
 function About() {
   return (
@@ -7,7 +9,7 @@ function About() {
       <h2 className="western text-6xl my-6">ABOUT</h2>
       <div className="flex flex-wrap justify-center gap-6 items-center">
         <div className="headshot border-double border-4 border-[#0b132b]">
-          <img src={flagHeadshot} alt="headshot with flag" />
+          <img src={headshot} alt="headshot with flag" />
         </div>
         <p className="border-double border-4 border-[#0b132b] rounded-lg w-96 sans bg-[#f7f3e3] p-2">Full Stack Web Developer with background in government contracting and international education. Ever curious and seeking new challenges. Experienced in working collaboratively to solve complex problems with tight timelines. Using 4+ years spent living and teaching in Togo, West Africa and Santiago, Chile to bring a unique perspective to the workplace.</p>
         <a href="https://docs.google.com/document/d/1gET7hNwH0Rk8XgEjFeRLcy6e-_OXkNoK/edit?usp=sharing&ouid=103959381080545579368&rtpof=true&sd=true" target="_blank" rel="noopener noreferrer">
